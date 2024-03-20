61146f021b0cb559ab8aaee5900c780ca65f590f
Antonio Vieira <vieira.n55@gmail.com>
2024-03-20 17:08:57 -0300

    refactor: perfil css

--- src/componentes/Perfil/index.tsx
+++ src/componentes/Perfil/index.tsx
@@ -4,15 +4,11 @@ import { useInView } from "react-intersection-observer";
 
 export default function Perfil(){
 
-  const [ref, inView] = useInView({
-    threshold: 0,
-  });
+
 
   return(
     <div 
-      ref={ref}
-      // className={estilos.perfil}
-      className={`${inView ? estilos.perfil2 : estilos.perfil}`}
+      className={estilos.perfil}
     >
       <div className={estilos.perfil__conteiner__avatar}>
         <img className={estilos.perfil__avatar} src={avatar} alt="Minha caracteristicas fisicas" />
