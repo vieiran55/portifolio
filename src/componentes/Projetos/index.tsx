ff8ac5a48f314213c58b528c795a22e3c30c6f56
Antonio Vieira <vieira.n55@gmail.com>
2024-03-21 16:34:45 -0300

    refactor: projetos css adjusted

--- src/componentes/Projetos/index.tsx
+++ src/componentes/Projetos/index.tsx
@@ -1,37 +1,9 @@
 import estilos from "./Projetos.module.scss";
-import projetos from "../../dados/projetos.json";
-import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
-import { GrLinkNext, GrLinkPrevious } from "react-icons/gr";
-import { useEffect, useState } from "react";
-import classNames from "classnames";
-import { HiMoon } from "react-icons/hi";
-import Box from "@mui/material/Box";
-import Slider from "@mui/material/Slider";
-import { Tooltip } from "@mui/material";
-import {
-  TbCircleNumber1,
-  TbCircleNumber2,
-  TbCircleNumber3,
-  TbCircleNumber4,
-  TbCircleNumber5,
-  TbCircleNumber6,
-  TbCircleNumber7,
-  TbCircleNumber8,
-  TbCircleNumber9,
-} from "react-icons/tb";
+import { useState } from "react";
+import dados from "../../dados/projetos.json";
 import { Link } from "react-router-dom";
-import { BsGithub } from "react-icons/bs";
-import { TbHomeHand } from "react-icons/tb";
-import { HiSun } from "react-icons/hi";
-import { useInView } from "react-intersection-observer";
 
 export default function Projetos() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
-  const [animaFadeDown, setAnimaFadeDown] = useState(false);
-  const [animaFadeLeft, setAnimaFadeLeft] = useState(false);
-
-
   const skillColors: { [key: string]: string } = {
     html: estilos.html,
     css: estilos.css,
@@ -54,8 +26,23 @@ export default function Projetos() {
         <h2 className={estilos.projetos__titulo2}>( )</h2>
       </div>
 
-      <div className={estilos.conteiner}>
-        Aqui serão inseridos os projetos
+      <div className={estilos.projetos__conteiner}>
+        <div className={estilos.projetos__conteiner__itens}>
+          {dados.map((item, index) => (
+            <div key={index} className={estilos.projetos__conteiner__itens__item}>
+              <div
+                className={estilos.projetos__conteiner__itens__box}
+              >
+                <div className={estilos.projetos__conteiner__itens__box__in}>
+                  <img src={item.image} alt={item.titulo} className={estilos.projetos__conteiner__itens__box__in__img}/>
+                </div>
+              </div>
+              <div className={estilos.projetos__conteiner__link}>
+                <Link to={item.site}>VER PROJETO</Link>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
