678267c53191fe9a92f4a856e47b7292a422f1a7
Antonio Vieira <vieira.n55@gmail.com>
2024-03-25 14:50:05 -0300

    refactor: servicos conteiner adjusted

--- src/componentes/Servicos/index.tsx
+++ src/componentes/Servicos/index.tsx
@@ -18,42 +18,82 @@ export default function Servicos() {
           </div>
         </div>
         <div className={estilos.servicos__conteudo}>
-          <div style={{display:"none"}}>
-            <img src={modelo} alt="modelo" className={estilos.servicos__modelo}/>
+          <div className={estilos.servicos__conteudo__cabecalho__desktop}>
+            <h1 className={estilos.servicos__conteudo__titulo__desktop}>
+              Precisa transformar sua
+              <strong className={estilos.servicos__conteudo__destaque__desktop}>
+                presença online?
+              </strong>
+            </h1>
+            <h1 className={estilos.servicos__conteudo__titulo__desktop}>Conheça nossos serviços:</h1>
           </div>
           <div className={estilos.servicos__conteudo__conteiner}>
             <div className={estilos.servicos__conteudo__cabecalho}>
               <h1 className={estilos.servicos__conteudo__titulo}>
-                Por que escolher transformar sua 
+                Precisa transformar sua
                 <strong className={estilos.servicos__conteudo__destaque}>
-                presença online 
+                  presença online?
                 </strong>
-                com Antonio Vieira Dev?
               </h1>
+              <h1>Conheça nossos serviços:</h1>
             </div>
-            <div className={estilos.servicos__conteudo__modelo__cont}>
-              <img src={modelo} alt="modelo" className={estilos.servicos__conteudo__modelo}/>
+            <div className={estilos.servicos__conteudo__modelo__cont__desktop}>
             </div>
-            <div className={estilos.servicos__conteudo__conteiner__cards}>
-              <img src={siteIcon} alt="" className={estilos.servicos__conteudo__conteiner__cards__icon}/>
-              <h2 className={estilos.servicos__conteudo__conteiner__cards__titulos}>Desenvolvimento de Sites:</h2>
-              <p>
-              Sites personalizados que destacam sua marca
-              </p>
-            </div>
-            <div className={estilos.servicos__conteudo__conteiner__cards}>
-              <img src={designIcon} alt="" className={estilos.servicos__conteudo__conteiner__cards__icon}/>
-              <h2 className={estilos.servicos__conteudo__conteiner__cards__titulos}>Design:</h2>
-              <p>
-              Design impactante para elevar sua identidade visual.
-              </p>
-            </div>
-            <div className={estilos.servicos__conteudo__conteiner__cards}>
-              <img src={appIcon} alt="" className={estilos.servicos__conteudo__conteiner__cards__icon}/>
-              <h2 className={estilos.servicos__conteudo__conteiner__cards__titulos}>Desenvolvimento de Aplicativos:</h2>
-              <p>
-              Aplicativos intuitivos que transformam ideias em experiências.
-              </p>
+            <div className={estilos.servicos__conteudo__modelo__box}>
+              <div className={estilos.servicos__conteudo__modelo__cont}>
+                <img
+                  src={modelo}
+                  alt="modelo"
+                  className={estilos.servicos__conteudo__modelo}
+                />
+              </div>
+              <div className={estilos.servicos__conteudo__conteiner__cards}>
+                <img
+                  src={siteIcon}
+                  alt=""
+                  className={estilos.servicos__conteudo__conteiner__cards__icon}
+                />
+                <h2
+                  className={
+                    estilos.servicos__conteudo__conteiner__cards__titulos
+                  }
+                >
+                  Desenvolvimento de Sites:
+                </h2>
+                <p>Sites personalizados que destacam sua marca</p>
+              </div>
+              <div className={estilos.servicos__conteudo__conteiner__cards}>
+                <img
+                  src={designIcon}
+                  alt=""
+                  className={estilos.servicos__conteudo__conteiner__cards__icon}
+                />
+                <h2
+                  className={
+                    estilos.servicos__conteudo__conteiner__cards__titulos
+                  }
+                >
+                  Design:
+                </h2>
+                <p>Design impactante para elevar sua identidade visual.</p>
+              </div>
+              <div className={estilos.servicos__conteudo__conteiner__cards}>
+                <img
+                  src={appIcon}
+                  alt=""
+                  className={estilos.servicos__conteudo__conteiner__cards__icon}
+                />
+                <h2
+                  className={
+                    estilos.servicos__conteudo__conteiner__cards__titulos
+                  }
+                >
+                  Desenvolvimento de Aplicativos:
+                </h2>
+                <p>
+                  Aplicativos intuitivos que transformam ideias em experiências.
+                </p>
+              </div>
             </div>
           </div>
         </div>
